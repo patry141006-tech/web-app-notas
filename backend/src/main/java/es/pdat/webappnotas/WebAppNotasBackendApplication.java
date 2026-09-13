3d356afefbc02c74db950417f6ee70f1632c213b
Daniela Alvarez <248350974+patry141006-tech@users.noreply.github.com>
2026-09-13 20:38:45 +0200

    crear aplicación Spring Boot y endpoint de bienvenida

---
+++ backend/src/main/java/es/pdat/webappnotas/WebAppNotasBackendApplication.java
@@ -0,0 +1,13 @@
+package es.pdat.webappnotas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppNotasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppNotasBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/es/pdat/webappnotas/WebAppNotasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package es.pdat.webappnotas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppNotasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
